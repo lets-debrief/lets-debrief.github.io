d02974c7d578fbb2c43df24c600ad48ce0c2ca4f
lets-debrief <sanjana.nambiar04@gmail.com>
2026-03-12 01:44:01 +0400

    Update render.js

--- render.js
+++ render.js
@@ -31,7 +31,6 @@
 
     const meta = document.createElement('div');
     meta.className = 'meta';
-    meta.innerHTML = `<span>${story.author}</span><span>${story.date}</span>`;
 
     article.appendChild(label);
     article.appendChild(content);
@@ -63,11 +62,6 @@
         "@type": "NewsArticle",
         headline: story.title,
         url: new URL(story.url, location.href).href,
-        datePublished: story.date,
-        author: {
-          "@type": "Person",
-          name: story.author,
-        },
         description: story.summary,
       },
     })),
